f41c3012d418d88c37acceabe34f0b0bf2e8404a
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-02-26 08:28:24 +0530

    Create e.py

---
+++ Python/e.py
@@ -0,0 +1,22 @@
+#Capatalize
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    string = s.split(' ')
+    newstring = (((i.capitalize() for i in string)))
+    return ' '.join(newstring)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
